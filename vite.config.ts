572f4595357895a5d48fbf94f21ed6ce65123866
Fouzan Naseer <fouzannaseer7@gmail.com>
2026-09-16 22:24:40 +0530

    Fix Vite alias resolution

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,13 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
-export default defineConfig({})
+export default defineConfig({
+  plugins: [react()],
+  base: "/Portfolio/",
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
